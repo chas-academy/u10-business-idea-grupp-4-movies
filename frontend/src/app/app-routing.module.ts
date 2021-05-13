44d393f7e7dfa7db564ec21e741758eba28acfe1
frankemon <frankie.hugo@hotmail.com>
2021-05-14 00:59:39 +0200

    add new route for landingpage, add to default / on start, style fix on forms, navbar, add userpage

--- frontend/src/app/app-routing.module.ts
+++ frontend/src/app/app-routing.module.ts
@@ -4,12 +4,14 @@ import { RouterModule, Routes } from '@angular/router';
 import { SigninComponent } from './components/login/login.component';
 import { SignupComponent } from './components/register/register.component';
 import { UserProfileComponent } from './components/userpage/userpage.component';
+import { LandingpageComponent } from './components/landingpage/landingpage.component';
 
 const routes: Routes = [
-    { path: '', redirectTo: '/login', pathMatch: 'full' },
+    { path: '', redirectTo: '/landingpage', pathMatch: 'full' },
     { path: 'login', component: SigninComponent },
     { path: 'register', component: SignupComponent },
     { path: 'profile', component: UserProfileComponent },
+    { path: 'landingpage', component: LandingpageComponent }
 ];
 
 @NgModule({
