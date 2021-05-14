72637264f4bb93ba393f095177640f15413538ba
frankemon <frankie.hugo@hotmail.com>
2021-05-14 13:51:08 +0200

    fix landingpage url on load, change component names to better suit routes

--- frontend/src/app/components/addfriends/addfriends.component.spec.ts
+++ frontend/src/app/components/addfriends/addfriends.component.spec.ts
@@ -1,25 +1,24 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
 
-import { AddfriendsComponent } from './addfriends.component';
+import { AllUsers } from './addfriends.component';
 
-describe('AddfriendsComponent', () => {
-  let component: AddfriendsComponent;
-  let fixture: ComponentFixture<AddfriendsComponent>;
+describe('AllUsers', () => {
+    let component: AllUsers;
+    let fixture: ComponentFixture<AllUsers>;
 
-  beforeEach(async () => {
-    await TestBed.configureTestingModule({
-      declarations: [ AddfriendsComponent ]
-    })
-    .compileComponents();
-  });
+    beforeEach(async () => {
+        await TestBed.configureTestingModule({
+            declarations: [AllUsers],
+        }).compileComponents();
+    });
 
-  beforeEach(() => {
-    fixture = TestBed.createComponent(AddfriendsComponent);
-    component = fixture.componentInstance;
-    fixture.detectChanges();
-  });
+    beforeEach(() => {
+        fixture = TestBed.createComponent(AllUsers);
+        component = fixture.componentInstance;
+        fixture.detectChanges();
+    });
 
-  it('should create', () => {
-    expect(component).toBeTruthy();
-  });
+    it('should create', () => {
+        expect(component).toBeTruthy();
+    });
 });
