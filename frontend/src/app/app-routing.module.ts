4605405bff70e4c30daf9c1a79339f3c073430aa
frankemon <frankie.hugo@hotmail.com>
2021-05-14 17:36:04 +0200

    add ui swipepage

--- frontend/src/app/app-routing.module.ts
+++ frontend/src/app/app-routing.module.ts
@@ -6,6 +6,7 @@ import { RegisterComponent } from './components/register/register.component';
 import { UserProfileComponent } from './components/userpage/userpage.component';
 import { LandingpageComponent } from './components/landingpage/landingpage.component';
 import { AllUsersComponent } from './components/allusers/allusers.component';
+import { SwipepageComponent } from './components/swipepage/swipepage.component';
 
 const routes: Routes = [
     { path: '', component: LandingpageComponent, pathMatch: 'full' },
@@ -13,6 +14,7 @@ const routes: Routes = [
     { path: 'register', component: RegisterComponent },
     { path: 'profile', component: UserProfileComponent },
     { path: 'users', component: AllUsersComponent },
+    { path: 'swipe', component: SwipepageComponent },
 ];
 
 @NgModule({
