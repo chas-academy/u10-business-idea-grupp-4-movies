221782a6f34e65c38e58ce8f9abb16ca9a95b49d
frankemon <frankie.hugo@hotmail.com>
2021-05-18 15:37:32 +0200

    remove route import, isSignedIn, error message from appcomponent

--- frontend/src/app/app.component.ts
+++ frontend/src/app/app.component.ts
@@ -1,5 +1,4 @@
 import { Component, OnInit } from '@angular/core';
-import { Router } from '@angular/router';
 
 @Component({
     selector: 'app-root',
@@ -7,10 +6,6 @@ import { Router } from '@angular/router';
     styleUrls: ['./app.component.scss'],
 })
 export class AppComponent implements OnInit {
-    // isSignedIn: boolean;
-
     constructor() {}
-    ngOnInit(): void {
-        //throw new Error('Method not implemented.');
-    }
+    ngOnInit(): void {}
 }
