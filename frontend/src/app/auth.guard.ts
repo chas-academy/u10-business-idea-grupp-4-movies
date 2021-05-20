d39774b089f1df5b3c094ed9a890ed52073f81e3
frankemon <frankie.hugo@hotmail.com>
2021-05-20 21:59:11 +0200

    add guard to swipe, profile, users

---
+++ frontend/src/app/auth.guard.ts
@@ -0,0 +1,30 @@
+import { Injectable } from '@angular/core';
+import {
+    ActivatedRouteSnapshot,
+    CanActivate,
+    Router,
+    RouterStateSnapshot,
+    UrlTree,
+} from '@angular/router';
+import { AuthStateService } from '../services/auth-state.service';
+import { map } from 'rxjs/operators';
+
+@Injectable({
+    providedIn: 'root',
+})
+export class AuthGuard implements CanActivate {
+    constructor(private auth: AuthStateService, private router: Router) {}
+
+    canActivate() {
+        return this.auth.userAuthState.pipe(
+            map((isSignedIn) => {
+                console.log(isSignedIn);
+                if (isSignedIn) {
+                    return true;
+                } else {
+                    return this.router.createUrlTree(['login']);
+                }
+            })
+        );
+    }
+}
