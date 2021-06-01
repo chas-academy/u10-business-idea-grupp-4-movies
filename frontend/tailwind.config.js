26e2ddadd9913c6064e4cd218feb962e1fde0539
frankemon <frankie.hugo@hotmail.com>
2021-06-01 20:32:03 +0200

    add twcss variables prep for styling

--- frontend/tailwind.config.js
+++ frontend/tailwind.config.js
@@ -1,11 +1,16 @@
 module.exports = {
-  purge: [],
-  darkMode: false, // or 'media' or 'class'
-  theme: {
-    extend: {},
-  },
-  variants: {
-    extend: {},
-  },
-  plugins: [],
-}
+    purge: [],
+    darkMode: false, // or 'media' or 'class'
+    theme: {
+        extend: {
+            colors: {
+                primary: '#0F1234',
+                secondary: '#E4345E',
+            },
+        },
+    },
+    variants: {
+        extend: {},
+    },
+    plugins: [],
+};
